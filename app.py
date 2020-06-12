29ce788cee0629a0c1086dee1f7b0518bfda7d5c
Slava <slavkostrov@gmail.com>
2020-06-12 17:08:06 +0300

    Bind Try

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from tkinter import *
 
+def hand1(event):
+    print("Hello world")
+
 root = Tk()
 
-w = Label(root, text = "Hello, world!")
-w.pack()
+label_1 = Label(root, text = "Hello, world!", font = "Times 50")
+
+
+label_1.pack()
 
 root.mainloop()
