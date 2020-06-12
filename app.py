0d80d3127396d35bb390ade481fa8321af8bc87d
Slava <slavkostrov@gmail.com>
2020-06-12 19:27:57 +0300

    Resize

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from tkinter import *
 
 def hand1(event):
-    label_1.config(text = "LOOOOl")
-    # print("test")
+    label_1.config(text = "LOOOOL")
 
 def hand2(event):
     label_1.config(text = "Hello world")
-    # print("test")
+
+def hand3(event):
+    label_1.config(fg = 'Blue', bg = 'Yellow')
 
 root = Tk()
 root.title("First program on Python")
 root.geometry('500x500')
+root.resizable(0, 0)
 
 label_1 = Label(root, text = "Hello, world!", font = "Times 20")
 label_1.grid(column = 0, row = 0)
@@ -20,6 +22,6 @@ button_1 = Button(root, text = "Run", bg = 'gray')
 button_1.place(x = 150 , y = 400, width = 200, height = 50)
 button_1.bind('<Enter>', hand1)
 button_1.bind('<Leave>', hand2)
-
+button_1.bind('<Button-1>', hand3)
 
 root.mainloop()
