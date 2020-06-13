08d1fdc484773df2c2059644a8aa51bbb808f65b
Slava <slavkostrov@gmail.com>
2020-06-13 23:00:40 +0300

    __init__

--- class.py
+++ class.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import randint
 
 
 root = tk.Tk()
@@ -7,16 +8,29 @@ root.title('Mini-game')
 root.resizable(0, 0)
 
 
-
-
-
-
+class Ball:
+    global area
+    x, y, r, dx, dy = 0, 0, 0, 0, 0
+    def __init__(self):
+        self.r = randint(25, 150)
+        self.x = randint(1 + self.r, 500 - self.r)
+        self.y = randint(1 + self.r, 500 - self.r)
+        self.dx = 5
+        self.dy = 5
+        print('Ball created')
+    
+    def show_on_canvas(self):
+        area.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = 'floral white', outline = 'blue')
 
 
 
 def main():
-    area = tk.Canvas(root, bg = 'yellow')
+    global area
+    area = tk.Canvas(root, bg = 'grey')
     area.pack(expand = True, fill = tk.BOTH)
+    
+    Ball().show_on_canvas()
+
     root.mainloop()
 
 main()
