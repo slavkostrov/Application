b55113eec3bf74770ec601eaa97e22e2dc74588d
Slava <slavkostrov@gmail.com>
2020-06-13 21:37:14 +0300

    Builded version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
 def hand1(event):
@@ -11,28 +11,35 @@ def hand3(event):
     label_1.config(fg = 'Blue', bg = 'Yellow')
 
 def hand4(event):
-    first = int(inp_1.get())
-    second = int(inp_2.get())
-    inp_1.delete(0, 'end')
-    inp_2.delete(0, 'end')
-    messagebox.showinfo("Result", str(first + second))
+    if inp_1.index('end') != 0 and inp_2.index('end') != 0:
+        first, second = int(inp_1.get()), int(inp_2.get())
+        inp_1.delete(0, 'end')
+        inp_2.delete(0, 'end')
+        inp_1.insert(0, '0')
+        inp_2.insert(0, '0')
+        messagebox.showinfo("Result", str(first + second))
+    else:
+        messagebox.showinfo("Error", 'Entry values') 
 
 
-root = Tk()
+root = tk.Tk()
 root.title("First program on Python")
 root.geometry('500x500')
 root.resizable(0, 0)
 
-inp_1 = Entry(root, width = 20)
+
+inp_1 = tk.Entry(root, width = 20)
+inp_1.insert(1, '0')
 inp_1.place(x = 200, y = 200, width = 50 , height = 25)
-inp_2 = Entry(root, width = 20)
+inp_2 = tk.Entry(root, width = 20, text = '0')
+inp_2.insert(1, '0')
 inp_2.place(x = 250, y = 200, width = 50, height = 25)
 
 
-label_1 = Label(root, text = "Hello, world!", font = "Times 20")
+label_1 = tk.Label(root, text = "Hello, world!", font = "Times 20")
 label_1.grid(column = 0, row = 0)
 
-button_1 = Button(root, text = "Run", bg = 'gray')
+button_1 = tk.Button(root, text = "Run", bg = 'gray')
 
 button_1.place(x = 150 , y = 400, width = 200, height = 50)
 button_1.bind('<Enter>', hand1)
