729fd207a42b3f5012edc355d0e391ef15a5fbed
Slava <slavkostrov@gmail.com>
2020-06-15 19:50:59 +0300

    Deleting shapes

--- class.py
+++ class.py
@@ -8,19 +8,24 @@ root.title('Mini-game')
 root.resizable(0, 0)
 
 
+def hand(event, ball):
+    area.delete(ball.id_ball)
+
 class Ball:
     global area
-    x, y, r, dx, dy = 0, 0, 0, 0, 0
+    id_ball, x, y, r, dx, dy = 0, 0, 0, 0, 0, 0
     def __init__(self):
         self.r = randint(25, 150)
         self.x = randint(1 + self.r, 500 - self.r)
         self.y = randint(1 + self.r, 500 - self.r)
         self.dx = 5
         self.dy = 5
+        self.id_ball = 0
+        area.bind('<Button-1>', lambda event, b = self: hand(event, b))
         print('Ball created')
     
     def show_on_canvas(self):
-        area.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = 'floral white', outline = 'blue')
+        self.id_ball = area.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = 'floral white', outline = 'blue')
 
 
 
@@ -29,7 +34,8 @@ def main():
     area = tk.Canvas(root, bg = 'grey')
     area.pack(expand = True, fill = tk.BOTH)
     
-    Ball().show_on_canvas()
+    a = Ball()
+    a.show_on_canvas()
 
     root.mainloop()
 
